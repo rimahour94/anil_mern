03d090f1a3127fd700ce869c6205366427ce2fa2
RITESH MAHOUR <riteshmahour94@gmail.com>
2022-12-23 09:59:00 +0530

    newchanges added

---
+++ anil/src/App.js
@@ -0,0 +1,39 @@
+function App() {
+  const dt = new Date();
+  console.log(dt);
+  const hr = dt.getHours();
+  const min = dt.getMinutes();
+  let text = "";
+  console.log(hr, min);
+
+  if (hr < 12) {
+    text = "Good Morning";
+  } else if (hr >= 12 && hr < 16) {
+    text = "Good Afternoon";
+  } else if (hr >= 16 && hr < 20) {
+    text = "Good Evening";
+  } else if (hr >= 20) {
+    text = "Good Night";
+  }
+
+  return (
+    <>
+      <h2>{text}</h2>
+    </>
+  );
+}
+
+// function AppOne() {
+//   return (
+//     <>
+//       <h2>App One</h2>
+//       <p>THis is Paragraph</p>
+//       <button>New</button>
+//     </>
+//   );
+// }
+
+export default App;
+// export { AppOne }; // naming export
+// there are two types of exports default/naming export
+//only one default export allowed per module.
--- anil/src/index.js
+++ anil/src/index.js
@@ -1,5 +1,12 @@
+import React from "react";
 import ReactDom from "react-dom/client";
-
+import App from "./App";
+// import { AppOne } from "./App";
 const root = ReactDom.createRoot(document.getElementById("root"));
 
-root.render(<h2>Welcome React WebPage</h2>);
+root.render(
+  <React.Fragment>
+    <App />
+    {/* <AppOne /> */}
+  </React.Fragment>
+);
